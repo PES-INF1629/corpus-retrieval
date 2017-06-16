3db1146084d7e3113b5a65166c9fed5b4d7b1c4d
Thiago Lages de Alencar <thiagola92@gmail.com>
2017-06-16 10:54:06 -0300

    Revert "Passando os valores da pegos da página html"
    This reverts commit 1acc33c8bfdc30e140e076f1e88c1c0ac801dea8.

--- app/workers/readmes_set_creator_worker.rb
+++ app/workers/readmes_set_creator_worker.rb
@@ -2,13 +2,7 @@ class ReadmesSetCreatorWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
 
-  def perform(query, match, label, comments)
-    puts(query)
-    puts(match)
-    puts(label)
-    puts(comments)
-    print("\nHERE\n")
-    sleep(60)
+  def perform(query)
     filename = query.gsub(/ +/, "_") + ".zip"
     readmes_set = ReadmesSet.create query: query, filename: filename, worker_id: self.jid
     ReadmesSet.destroy_olds!
