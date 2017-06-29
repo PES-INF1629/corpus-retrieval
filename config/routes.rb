7a4e64d6cad7e3e8d3d267cb6dfb78e5c044bb8d
Thiago Lages de Alencar <thiagola92@gmail.com>
2017-06-29 19:42:15 -0300

    Esqueci dessas partes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
-  root to: 'readmes#index', as: :readmes
-  resources :readmes, only: [] do
+  root to: 'issues#index', as: :issues
+  resources :issues, only: [] do
     get :download
     post :search, on: :collection
   end
