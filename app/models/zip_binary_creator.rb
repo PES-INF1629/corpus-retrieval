5a5b0dc0b69050ea3b450de2f6b533e053bff404
Jordan2R <rrodrigues_jordan@hotmail.com>
2017-07-04 19:02:10 -0300

    Zip with jsons almost finished

--- app/models/zip_binary_creator.rb
+++ app/models/zip_binary_creator.rb
@@ -5,12 +5,49 @@ module ZipBinaryCreator
     zipOutputBuffer = Zip::OutputStream.write_buffer do |zipElement|
       files.each do |file|
         filename = file[:filename]
-        content = file[:content]
+        content = create_json file
         zipElement.put_next_entry filename
         zipElement.write content
       end
     end
     zipOutputBuffer.rewind
     zipOutputBuffer.sysread
+  end
+
+  def create_json(file)
+	jsonFile = "{\n"
+
+	jsonFile += "\"url\": \"#{file[:url]}\",\n"
+	jsonFile += "\"title\": \"#{file[:title]}\",\n"
+	jsonFile += "\"user\": \"#{file[:user]}\",\n"
+	jsonFile += "\"labels\": [\n"
+
+	file[:labels].each_with_index { |label, index|
+		jsonFile += "\t{"
+		
+		jsonFile += "\t\t\"name\": \"#{label[:name]}\"\n"
+
+		if index == file[:labels].length - 1 then
+			jsonFile += "\t}\n"
+		else
+			jsonFile += "\t},\n"
+		end
+	}
+
+	if file[:comments].nil? then
+		jsonFile += "\"body\": \"#{file[:body]}\"\n"
+	else
+		jsonFile += "\"body\": \"#{file[:body]}\",\n"
+
+		jsonFile += "\"comments\": #{file[:comments]},\n"
+		jsonFile += "\"comments_content\": [\n"
+
+
+		#file[:comments_content].each_with_index { |comments_content, index|
+
+		jsonFile += "]\n"
+	end
+
+	jsonFile += "}"
   end
 end
